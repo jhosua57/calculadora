95ee9f981a338050d764e3a86fb15ea82a59dca0
jhosua57 <jhosua57@gmail.com>
2023-03-20 12:32:32 -0400

    Creacion de builds

---
+++ docs/precache-manifest.62bf106bba35de067182c451a2da3009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7b961652cc05f708852",
+    "url": "/static/css/main.4381231e.chunk.css"
+  },
+  {
+    "revision": "a7b961652cc05f708852",
+    "url": "/static/js/main.a7b96165.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "6a07f7a91c7d17f837d7",
+    "url": "/static/js/2.6a07f7a9.chunk.js"
+  },
+  {
+    "revision": "4a4fcd1f8f9790e96d57",
+    "url": "/static/js/runtime~main.4a4fcd1f.js"
+  },
+  {
+    "revision": "960e8706fd4c007a1a93c9a721bec346",
+    "url": "/index.html"
+  }
+];
